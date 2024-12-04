4ed3dfa9b1c7c470e0f12114476faafdfac528c9
Vishal Mishra <84886848+vishal7788-code@users.noreply.github.com>
2024-12-04 08:04:25 +0530

    modifying vite config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,12 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import eslintPlugin from "vite-plugin-eslint"; // Import the ESLint plugin
-import { defineConfig } from "vite";
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [
-    react(),
-    eslintPlugin({
-      emitWarning: false, // Disable warnings in the terminal
-      emitError: false,   // Disable errors in the terminal
-      failOnWarning: false, // Ensure the build doesn't fail on warnings
-      failOnError: false,   // Ensure the build doesn't fail on errors
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
