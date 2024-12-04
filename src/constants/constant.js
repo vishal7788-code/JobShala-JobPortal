dc8164db9610fe5e6a502a0207d7676b730b7a66
Vishal Mishra <vm174294@gmail.com>
2024-12-05 00:41:19 +0530

    resolved constants conflicts

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,5 +1,14 @@
+export const USER_API_END_POINT =
+  process.env.NODE_ENV === "production"
+    ? "https://jobshala-backend.onrender.com/api/v1/user"
+    : "http://localhost:8001/api/v1/user";
 
-export const USER_API_END_POINT = "https://jobshala-backend.onrender.com/api/v1/user";
-export const JOB_API_END_POINT = "https://jobshala-backend.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT = "https://jobshala-backend.onrender.com/api/v1/application";
+export const JOB_API_END_POINT =
+  process.env.NODE_ENV === "production"
+    ? "https://jobshala-backend.onrender.com/api/v1/job"
+    : "http://localhost:8001/api/v1/job";
 
+export const APPLICATION_API_END_POINT =
+  process.env.NODE_ENV === "production"
+    ? "https://jobshala-backend.onrender.com/api/v1/application"
+    : "http://localhost:8001/api/v1/application";
