926237be2c20633b8c639d3c7ec9cdfa718ecbc5
Vishal Mishra <84886848+vishal7788-code@users.noreply.github.com>
2024-12-04 18:01:38 +0530

    Update constant.js

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -5,7 +5,3 @@ export const APPLICATION_API_END_POINT = "https://jobshala-backend.onrender.com/
 
 
 
-
-// export const USER_API_END_POINT = "https://localhost:8000/api/v1/user"
-// export const JOB_API_END_POINT = "https://localhost:8000/api/v1/job"
-// export const APPLICATION_API_END_POINT = "https://localhost:8000/v1/application"
